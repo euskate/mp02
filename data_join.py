28fe84fede4557b6445a29b5f03dbdb4f2b7cd1b
euskate <euskate07@gmail.com>
2020-02-17 20:52:04 +0900

    2/17 last

---
+++ data_join.py
@@ -0,0 +1,44 @@
+# 라이브러리 불러오기
+import pandas as pd
+import numpy as np
+
+# 데이터 불러오기
+df_moo = pd.read_csv('./data/result_df.csv')
+df_wheather = pd.read_csv('./data/date_final.csv')
+
+# 데이터 확인
+print(df_moo.head())
+print(df_moo.dtypes)
+print(df_wheather.head())
+print(df_wheather.dtypes)
+print(df_wheather.columns)
+
+# 컬럼명 변경
+df_moo.rename(columns={'date':'날짜'}, inplace=True)
+df_moo.columns
+
+# 데이터 타입 변경
+df_moo['날짜'] = pd.to_datetime(df_moo.날짜)
+df_wheather['날짜'] = pd.to_datetime(df_wheather.날짜)
+df_wheather['현재기온'].astype(np.int32)
+df_wheather['현재기온'] = pd.to_numeric(df_wheather['현재기온'], errors='coerce')
+df_wheather['이슬점온도'] = pd.to_numeric(df_wheather['이슬점온도'], errors='coerce')
+df_wheather['체감온도'] = pd.to_numeric(df_wheather['체감온도'], errors='coerce')
+df_wheather['강수량'] = pd.to_numeric(df_wheather['강수량'], errors='coerce')
+df_wheather['습도'] = pd.to_numeric(df_wheather['습도'], errors='coerce')
+df_wheather['풍속'] = pd.to_numeric(df_wheather['풍속'], errors='coerce')
+df_wheather['해면기압'] = pd.to_numeric(df_wheather['해면기압'], errors='coerce')
+
+# 확인
+print(df_wheather.columns)
+print(df_wheather.dtypes)
+print(df_moo.dtypes)
+
+# 데이터 병합
+df = pd.merge(df_moo, df_wheather, on='날짜')
+
+# 데이터 확인
+print(df.head())
+
+# CSV 파일로 출력
+df.to_csv('./data/join_moo_wheather.csv', encoding='utf-8', index=False)
---
+++ xlstoxlsx.py
@@ -0,0 +1,47 @@
+# import xlrd
+# from openpyxl.workbook import Workbook
+# from openpyxl.reader.excel import load_workbook, InvalidFileException
+
+# def open_xls_as_xlsx(filename):
+#     # first open using xlrd
+#     book = xlrd.open_workbook(filename)
+#     index = 0
+#     nrows, ncols = 0, 0
+#     while nrows * ncols == 0:
+#         sheet = book.sheet_by_index(index)
+#         nrows = sheet.nrows
+#         ncols = sheet.ncols
+#         index += 1
+
+#     # prepare a xlsx sheet
+#     book1 = Workbook()
+#     sheet1 = book1.get_active_sheet()
+
+#     for row in xrange(0, nrows):
+#         for col in xrange(0, ncols):
+#             sheet1.cell(row=row, column=col).value = sheet.cell_value(row, col)
+
+#     return book1
+
+# open_xls_as_xlsx('./2015_01_03.xls')
+
+# conda install -c conda-forge pyexcel 
+# conda install -c conda-forge pyexcel-xlsx 
+# conda install -c conda-forge pyexcel-xls 
+
+# import pyexcel as p 
+
+# p.save_book_as(file_name='2015_01_03.xls', dest_file_name='2015_01_03.xlsx')
+
+
+
+import os
+import win32com.client
+xl = win32com.client.Dispatch("Excel.Application")
+path =  os.getcwd().replace('\'','\\') + '\\'
+print(path)
+wb = xl.Workbooks.Open(path+"2015_01_03.xls")
+wb.SaveAs(path+"2015_01_03.xlsx", FileFormat = 51)
+wb.Close()
+xl.Quit()
+
