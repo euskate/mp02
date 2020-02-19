f4350f86ef2923339942f2ad8053e4f39d8b4449
euskate <euskate07@gmail.com>
2020-02-19 16:34:15 +0900

    2/19 css bootstrap

--- mp02_web/mp02/views.py
+++ mp02_web/mp02/views.py
@@ -8,14 +8,25 @@ class HomeView(TemplateView):
     template_name = 'index.html'
 
 def step1(request):
-    return render(request, 'step1.html')
+    context = {'title':'step1. 연구 목표 설정'}
+    return render(request, 'step1.html', context=context)
+
 def step2(request):
-    return render(request, 'step2.html')
+    context = {'title':'step2. 데이터 획득'}
+    return render(request, 'step2.html', context=context)
+
 def step3(request):
-    return render(request, 'step3.html')
+    context = {'title':'step3. 데이터 준비'}
+    return render(request, 'step3.html', context=context)
+
 def step4(request):
-    return render(request, 'step4.html')
+    context = {'title':'step4. 데이터 탐색'}
+    return render(request, 'step4.html', context=context)
+
 def step5(request):
-    return render(request, 'step5.html')
+    context = {'title':'step5. 데이터 모델링'}
+    return render(request, 'step5.html', context=context)
+
 def step6(request):
-    return render(request, 'step6.html')
+    context = {'title':'step6. 발표'}
+    return render(request, 'step6.html', context=context)
